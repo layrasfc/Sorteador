98fb0738b0d936cf4bd0199f294d8b57a9743158
Layra Fernandes <83982877+layrasfc@users.noreply.github.com>
2023-08-15 08:00:34 -0300

    Update sorteador.py

--- sorteador.py
+++ sorteador.py
@@ -1,6 +1,5 @@
-print("- - - Adivinhe o número - - -")
+print("- - - ADIVINHE O NÚMERO - - -")
 print("Regras:\n - O número sorteado está entre 0 a 100 e é um número par.\n - Você tem quatro chances e 200 de vida.\n - A cada erro você perderá o equivalente à diferença ao número sorteado.\n ")
-print("\n")
 
 import random
 tent = 4
@@ -14,20 +13,18 @@ while resto == 0:
 while tent > 0 and vida > 0:
     number = int(input("Digite o número: "))
     if number == n_sorteio:
-        print("Parabéns! Você acertou o número!")
+        print("--- PARABÉNS! ---\nVocê acertou o número!")
         break
     else:
-        print("Você errou")
         tent = tent - 1
         prox_vida = n_sorteio - number
         vida = vida - abs(prox_vida)
-        print(vida)
+        print("Você errou! A sua vida agora é:", vida, "\n")
         continue
 
 while tent == 0 or vida == 0:
-    print("Suas chances ou vida acabaram, boa sorte na próxima vez!")
+    print("--- FIM DE JOGO ---\nSuas chances ou vida acabaram, boa sorte na próxima vez!")
     break
-
 
 
 
