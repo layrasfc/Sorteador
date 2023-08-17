ad3bb069013506cd45a16a428f01bcaa92bac029
Layra Fernandes <83982877+layrasfc@users.noreply.github.com>
2023-08-17 11:21:15 -0300

    time_sorteador.py

--- sorteador.py
+++ sorteador.py
@@ -10,26 +10,35 @@ while resto == 0:
     n_sorteio = random.randrange(0, 100)
     resto = n_sorteio % 2
 
+import time
+tempo_comeco = time.time() #pega o tempo que começou
+tempo_fim = 60 #fim é 60
+
 while tent > 0 and vida > 0:
     print(tentativas, "º Tentativa")
     tentativas += 1
     number = int(input("Digite o número: "))
-    if number == n_sorteio:
-        print("--- PARABÉNS! ---\nVocê acertou o número!")
-        break
-    elif vida < 0:
-        vida = 0
-        break
+    tempo_agora = time.time()  # marca o tempo atual
+    tempo_fim = tempo_agora - tempo_comeco #diferença do começo até o momento q digitou o chute
+    print(f"Tempo: {tempo_fim:.02f}")
+    if tempo_fim < 60:
+        if number == n_sorteio:
+            print("--- PARABÉNS! ---\nVocê acertou o número!")
+            break
+        elif vida < 0:
+            vida = 0
+            break
+        else:
+            tent = tent - 1
+            vida = vida - abs(n_sorteio - number)
+            print("Você errou! A sua vida agora é:", vida, "\n")
+            continue
     else:
-        tent = tent - 1
-        vida = vida - abs(n_sorteio - number)
-        print("Você errou! A sua vida agora é:", vida, "\n")
-        continue
-
-while tent == 0 or vida <= 0:
-    print("--- FIM DE JOGO ---\nSuas chances ou vida acabaram, boa sorte na próxima vez!")
+        print("Seu tempo acabou!")
     break
 
 
-
+while tent == 0 or vida <= 0 or tempo_fim == 60:
+    print("--- FIM DE JOGO ---\nSuas chances ou vida acabaram, boa sorte na próxima vez!")
+    break
 
